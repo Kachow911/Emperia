ddefbb9bb38de869bb700adc3740a14b386fe671
ColinAV516 <colinav516@gmail.com>
2018-12-29 19:25:48 -0800

    Projectile Changes

--- Projectiles/MarbleEnergy.cs
+++ Projectiles/MarbleEnergy.cs
@@ -1,8 +1,8 @@
-using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using System;
 using Terraria.ID;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
 using Terraria.ModLoader;
 
 namespace Emperia.Projectiles
@@ -13,7 +13,7 @@ namespace Emperia.Projectiles
 		private int explodeRadius = 100;
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Energy-Filled Rock");
+			DisplayName.SetDefault("Marble Energy");
 		}
         public override void SetDefaults()
         {  //projectile name
@@ -30,27 +30,32 @@ namespace Emperia.Projectiles
 			projectile.alpha = 255;
         }
         public override void AI()           //projectile make that the projectile will face the corect way
-        {                                                           // |
-            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
-			int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 66, 0f, 0f, 91, new Color(255, 255, 255), 1.5f);
-            Main.dust[dust].velocity *= 0.1f;
-            if (projectile.velocity == Vector2.Zero)
-            {
-                Main.dust[dust].velocity.Y -= 1f;
-                Main.dust[dust].scale = 1.2f;
-            }
-            else
-            {
-                Main.dust[dust].velocity += projectile.velocity * 0.2f;
-            }
-            Main.dust[dust].position.X = projectile.Center.X + 4f + (float)Main.rand.Next(-2, 3);
-            Main.dust[dust].position.Y = projectile.Center.Y + (float)Main.rand.Next(-2, 3);
+        {                                                           
+			int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 66, 0f, 0f, 91, new Color(255, 255, 255), 1f);
+            Main.dust[dust].position += projectile.velocity.RotatedBy(1.57);
             Main.dust[dust].noGravity = true;
-		}
+            Main.dust[dust].velocity = projectile.velocity.RotatedBy(0.5);
+            int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 66, 0f, 0f, 91, new Color(255, 255, 255), 1f);
+            Main.dust[dust].position += projectile.velocity.RotatedBy(-1.57);
+            Main.dust[dust1].noGravity = true;
+            Main.dust[dust1].velocity = projectile.velocity.RotatedBy(-0.5);
+            int dust2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 66, 0f, 0f, 91, new Color(255, 255, 255), 1.5f);
+            Main.dust[dust2].velocity = projectile.velocity;
+            Main.dust[dust2].noGravity = true;
+
+        }
 		public override void Kill(int timeLeft)
         {
-			//
-		}
+			for (int i = 0; i < 360; i += 10)
+			{
+				Vector2 vec = Vector2.Transform(new Vector2(-10, 0), Matrix.CreateRotationZ(MathHelper.ToRadians(i)));
+				vec.Normalize();
+				int num622 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 66, 0f, 0f, 91, new Color(255, 255, 255), 1.5f);
+                Main.dust[num622].velocity += (vec * 2f);
+                Main.dust[num622].velocity += projectile.velocity;
+                Main.dust[num622].noGravity = true;
+            }
+	     }
         
     }
 }
--- Projectiles/SandBlast.cs
+++ Projectiles/SandBlast.cs
@@ -22,22 +22,18 @@ namespace Emperia.Projectiles
             projectile.magic = true;         // 
             projectile.tileCollide = true;   //make that the projectile will be destroed if it hits the terrain
             projectile.penetrate = -1;      //how many npc will penetrate
-            projectile.timeLeft = 200;   //how many time projectile projectile has before disepire
-            projectile.light = 0.75f;    // projectile light
+            projectile.timeLeft = 120;
             projectile.extraUpdates = 1;
             projectile.ignoreWater = true;
 			projectile.alpha = 255;
         }
         public override void AI()           //projectile make that the projectile will face the corect way
-        {                                                           // |
+        {
+            projectile.velocity *= .98f;
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
-			 /*Dust dust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 231, 0.0f, 0.0f, 0, new Color(), 1f)];
-			dust.position = projectile.Center;
-			dust.velocity = Vector2.Zero;
-			dust.noGravity = true;*/
 			for (int i = 0; i < 2; i++)
 			{
-				int dust = Dust.NewDust(new Vector2(projectile.position.X, (float) ((double) projectile.position.Y + (double) projectile.height - 16.0)), projectile.width, 16, 85, 0.0f, 0.0f, 0, new Color(), 1f);
+				int dust = Dust.NewDust(new Vector2(projectile.position.X, (float) ((double) projectile.position.Y + (double) projectile.height - 16.0)), projectile.width, 16, 85, 0.0f, 0.0f, 0, new Color(), 1.5f);
 				Main.dust[dust].velocity *= 0.1f;
 				if (projectile.velocity == Vector2.Zero)
 				{
