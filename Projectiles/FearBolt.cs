2a10f3f876c0f5a7c342d396cbeea68b5e2a5ac1
Kachow911 <torniky@gmail.com>
2019-02-06 17:47:26 -0800

    inquisitorro

---
+++ Projectiles/FearBolt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace Emperia.Projectiles
+{
+    public class FearBolt : ModProjectile
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Fear Bolt");
+		}
+        public override void SetDefaults()
+        {
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.friendly = false;
+            projectile.tileCollide = true;
+            projectile.penetrate = 4;
+            projectile.timeLeft = 200;
+            projectile.light = 0.75f;
+            projectile.extraUpdates = 1;
+            projectile.ignoreWater = true;
+            projectile.hostile = true;
+        }
+
+        public override void AI()
+        {
+			projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X);
+
+            Dust.NewDust(projectile.Center, 2, 2, 58, projectile.velocity.X, projectile.velocity.Y);
+        }
+    }
+}
