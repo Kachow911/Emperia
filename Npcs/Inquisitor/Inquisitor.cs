a7fb66fea32c8071dac5895d6da0fab8d1bfe9b4
Kachow911 <torniky@gmail.com>
2019-02-07 17:37:18 -0800

    njjjklln

--- Npcs/Inquisitor/Inquisitor.cs
+++ Npcs/Inquisitor/Inquisitor.cs
@@ -13,7 +13,8 @@ namespace Emperia.Npcs.Inquisitor
         private enum Move
         {
             Hover,
-			Teleport
+			Teleport,
+			Shoot
         }
 
         private Move move { get { return (Move)npc.ai[0]; } set { npc.ai[0] = (int)value; } }
@@ -41,7 +42,7 @@ namespace Emperia.Npcs.Inquisitor
             npc.defense = 40;
             npc.knockBackResist = 0f;
             npc.width = 94;
-            npc.height = 101;
+            npc.height = 100;
 			npc.alpha = 0;
             npc.value = Item.buyPrice(0, 20, 0, 0);
             npc.npcSlots = 1f;
@@ -85,11 +86,11 @@ namespace Emperia.Npcs.Inquisitor
 			}
 			
 		
-            if (npc.ai[3] == 0)
+            if (npc.ai[3] == 0 && npc.life <= npc.lifeMax * .9)
             {
 				SetMove(Move.Hover, 240);
-                /*NPC.NewNPC((int)npc.Center.X - 20, (int)npc.Center.Y, mod.NPCType<SorrowMask>(), ai0: player.whoAmI, ai1: npc.whoAmI);
-                NPC.NewNPC((int)npc.Center.X + 20, (int)npc.Center.Y, mod.NPCType<AgonyMask>(), ai0: player.whoAmI, ai1: npc.whoAmI);*/
+                NPC.NewNPC((int)npc.Center.X - 20, (int)npc.Center.Y, mod.NPCType("EocPuppet"), ai0: player.whoAmI, ai1: npc.whoAmI);
+                /*NPC.NewNPC((int)npc.Center.X + 20, (int)npc.Center.Y, mod.NPCType<AgonyMask>(), ai0: player.whoAmI, ai1: npc.whoAmI);*/
                 npc.ai[3] = 1;
             }
 			if (move == Move.Hover)
@@ -99,31 +100,64 @@ namespace Emperia.Npcs.Inquisitor
 				npc.velocity.Y = 0.5f * (float)Math.Cos(MathHelper.ToRadians(counter * 2));
 				if (counter <= 0)
 				{
-					SetMove(Move.Teleport, 0);
+					if (Main.rand.NextBool(2))
+						SetMove(Move.Teleport, 0);
+					else
+						SetMove(Move.Shoot, 120);
 				}
 			}
 			if (move == Move.Teleport)
 			{
-				for (int i = 0; i < 50; ++i) //Create dust b4 teleport
-			    {
-					int dust = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
-					int dust1 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
-					Main.dust[dust1].scale = 1.5f;
-					Main.dust[dust1].velocity *= 1.5f;
-					int dust2 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
-					Main.dust[dust2].scale = 1.5f;
+				bool sucess = false;
+				while (!sucess)
+				{
+					Vector2 targPos = player.Center + new Vector2(200, 0).RotatedByRandom(MathHelper.ToRadians(360));
+					if (targPos.Y < player.Center.Y)
+					{
+						sucess = true;
+						for (int i = 0; i < 50; ++i) //Create dust b4 teleport
+						{
+							int dust = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
+							int dust1 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
+							Main.dust[dust1].scale = 1.5f;
+							Main.dust[dust1].velocity *= 1.5f;
+							int dust2 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
+							Main.dust[dust2].scale = 1.5f;
+						}
+						npc.position = targPos;
+						for (int i = 0; i < 50; ++i) //Create dust after teleport
+						{
+							int dust = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
+							int dust1 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
+							Main.dust[dust1].scale = 1.5f;
+							Main.dust[dust1].velocity *= 1.5f;
+							int dust2 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
+							Main.dust[dust2].scale = 1.5f;
+						}
+					}
+					SetMove(Move.Hover, 240);
 				}
-				npc.position = player.Center + new Vector2(200, 0).RotatedByRandom(MathHelper.ToRadians(360));
-				for (int i = 0; i < 50; ++i) //Create dust after teleport
-			    {
-					int dust = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
-					int dust1 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
-					Main.dust[dust1].scale = 1.5f;
-					Main.dust[dust1].velocity *= 1.5f;
-					int dust2 = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldCoin);
-					Main.dust[dust2].scale = 1.5f;
+			}
+			if (move == Move.Shoot)
+			{
+				npc.velocity = Vector2.Zero;
+				counter--;
+				for (int i = 0; i < 360; i+= 5)
+				{
+					int dust = Dust.NewDust(new Vector2(npc.Center.X , npc.Center.Y) + new Vector2(0, -100).RotatedBy(MathHelper.ToRadians(i)), npc.width / 8, npc.height / 8, 58, 0f, 0f, 0, Color.White, 1.5f);
+					Main.dust[dust].velocity = Vector2.Zero;
 				}
-				SetMove(Move.Hover, 240);
+				if (counter % 20 == 0)
+				{
+					Vector2 placePosition = npc.Center + new Vector2(0, -100).RotatedByRandom(MathHelper.ToRadians(360));
+					Vector2 direction = (Main.player[npc.target].Center - placePosition);
+					direction.Normalize();
+					Projectile.NewProjectile(placePosition.X, placePosition.Y, direction.X * 7f, direction.Y * 7f, mod.ProjectileType("FearBolt"), 30, 1, Main.myPlayer, 0, 0);	
+				}
+				if (counter <= 0)
+				{
+					SetMove(Move.Hover, 240);
+				}
 			}
 
 			
