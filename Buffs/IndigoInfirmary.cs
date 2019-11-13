58fd2fa94651f11bf8ab9521924ff2ca237b56b6
Miningdiamonds8 <smilecupcake8@gmail.com>
2019-11-13 17:20:53 -0500

    Update IndigoInfirmary.cs

--- Buffs/IndigoInfirmary.cs
+++ Buffs/IndigoInfirmary.cs
@@ -19,7 +19,7 @@ namespace Emperia.Buffs
 
         public override void Update(NPC npc, ref int buffIndex)
         {
-            npc.GetGlobalNPC<MyNPC>(mod).indigoInfirmary = true;    //this tells the game to use the public bool customdebuff from NPCsINFO.cs
+            npc.GetGlobalNPC<MyNPC>().indigoInfirmary = true;    //this tells the game to use the public bool customdebuff from NPCsINFO.cs
             int num1 = Dust.NewDust(npc.position, npc.width, npc.height, 21);    //this is the dust/flame effect that will apear on npc or player if is hit by this buff   
 			Main.dust[num1].noGravity = true;
 			Main.dust[num1].velocity *= 2f;
