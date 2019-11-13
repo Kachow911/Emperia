a56f1a30a0baded8a986c3df8172d5bcc173e90e
Miningdiamonds8 <smilecupcake8@gmail.com>
2019-11-12 23:25:01 -0500

    Update Dusk.cs

--- Items/Weapons/Color1/Dusk.cs
+++ Items/Weapons/Color1/Dusk.cs
@@ -6,48 +6,44 @@ using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Emperia.Items.Weapons.Color1   //where is located
+namespace Emperia.Items.Weapons.Color1
 {
     public class Dusk : ModItem
     {
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Dusk");
-			Tooltip.SetDefault("Striking an enemy will give you the 'Indigo Inertia' Buff");
+			Tooltip.SetDefault("Slicing an enemy will increase melee damage and life regeneration for a short time\nStriking a boss increases the duration\nShooting an enemy will deal damage over time that increases as your health rises");
 		}
         public override void SetDefaults()
         {   //Sword name
-            item.damage = 142;            //Sword damage
-            item.melee = true;            //if it's melee
-            item.width = 32;              //Sword width
-            item.height = 32;             //Sword height
-            item.useTime = 33;          //how fast 
+            item.damage = 126;
+            item.melee = true;
+            item.width = 32;
+            item.height = 32;
+            item.useTime = 33;
             item.useAnimation = 33;     
-            item.useStyle = 1;        //Style is how this item is used, 1 is the style of the sword
+            item.useStyle = 1;
             item.knockBack = 5.5f;  
-			item.crit = 4;			//Sword knockback
-            item.value = 100;        
+            item.value = 232500;        
             item.rare = 5;
 			item.UseSound = SoundID.Item1;
 			item.shoot = mod.ProjectileType("DuskProj");
 			item.shootSpeed = 16f;
 			item.scale = 1f;
-            item.autoReuse = true;   //if it's capable of autoswing.
-            item.useTurn = true;             //projectile speed                 
+            item.autoReuse = true;
+            item.useTurn = true;                
         }
 		
-        public override void AddRecipes()  //How to craft this sword
+        public override void AddRecipes()
         {
 			ModRecipe recipe = new ModRecipe(mod);      
             recipe.AddIngredient(null, "CeruleanClaymore", 1); 
 			recipe.AddIngredient(null, "IndigoIaito", 1); 
-			recipe.AddRecipeGroup("Emperia:PalBar", 4);
-			recipe.AddRecipeGroup("Emperia:AdBar", 4);
-			recipe.AddIngredient(ItemID.HallowedBar, 2); 
-			recipe.AddIngredient(ItemID.SoulofLight, 4); 
-			recipe.AddIngredient(ItemID.SoulofSight, 4); 
-			recipe.AddIngredient(ItemID.CrystalShard, 20);
-						
+			recipe.AddIngredient(null, "PearlyPrism", 1); 
+			recipe.AddIngredient(ItemID.SoulofMight, 4); 	
+			recipe.AddIngredient(ItemID.CobaltBar, 2); 
+			recipe.AddIngredient(ItemID.OrichalcumBar, 2); 
             recipe.AddTile(TileID.MythrilAnvil); 			
             recipe.SetResult(this);
             recipe.AddRecipe(); 
