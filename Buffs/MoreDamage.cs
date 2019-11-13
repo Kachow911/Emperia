6426951eddf8398cb23988b09dc5be28c8bb3337
Miningdiamonds8 <smilecupcake8@gmail.com>
2019-11-13 17:27:30 -0500

    Update MoreDamage.cs

--- Buffs/MoreDamage.cs
+++ Buffs/MoreDamage.cs
@@ -19,7 +19,7 @@ namespace Emperia.Buffs
 
         public override void Update(NPC npc, ref int buffIndex)
         {
-            npc.GetGlobalNPC<MyNPC>(mod).moreDamage = true;
+            npc.GetGlobalNPC<MyNPC>().moreDamage = true;
         }
         
 
