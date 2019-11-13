35d4a1a4e68294c66c67a5fa5dc01bfb9818ad85
Miningdiamonds8 <smilecupcake8@gmail.com>
2019-11-12 22:53:42 -0500

    Update Mellow.cs

--- Items/Weapons/Color1/Mellow.cs
+++ Items/Weapons/Color1/Mellow.cs
@@ -13,20 +13,19 @@ namespace Emperia.Items.Weapons.Color1   //where is located
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Mellow");
-			Tooltip.SetDefault("Striking an enemy will increase movement speed, melee speed and flight time for a short time");
+			Tooltip.SetDefault("Slicing an enemy will increase movement and melee speed for a short time\nStriking a boss increases the duration\nShooting an enemy will decrease their defense as your mph rises");
 		}
         public override void SetDefaults()
         {   //Sword name
             item.damage = 45;            //Sword damage
             item.melee = true;            //if it's melee
-            item.width = 32;              //Sword width
-            item.height = 32;             //Sword height
-            item.useTime = 10;          //how fast 
+            item.width = 46;              //Sword width
+            item.height = 46;             //Sword height
+            item.useTime = 13;          //how fast 
             item.useAnimation = 10;     
             item.useStyle = 1;        //Style is how this item is used, 1 is the style of the sword
             item.knockBack = 1.5f;  
-			item.crit = 4;			//Sword knockback
-            item.value = 100;        
+            item.value = 232500;        
             item.rare = 5;
 			item.UseSound = SoundID.Item1;
 			item.shoot = mod.ProjectileType("MellowSlice");
@@ -41,12 +40,10 @@ namespace Emperia.Items.Weapons.Color1   //where is located
 			ModRecipe recipe = new ModRecipe(mod);      
             recipe.AddIngredient(null, "BerylBlade", 1); 
 			recipe.AddIngredient(null, "SaffronSaber", 1); 
-			recipe.AddRecipeGroup("Emperia:PalBar", 4);
-			recipe.AddRecipeGroup("Emperia:AdBar", 4);
-			recipe.AddIngredient(ItemID.HallowedBar, 2); 
-			recipe.AddIngredient(ItemID.SoulofLight, 4); 
-			recipe.AddIngredient(ItemID.SoulofSight, 4); 
-			recipe.AddIngredient(ItemID.CrystalShard, 20);		
+			recipe.AddIngredient(null, "PearlyPrism", 1); 
+			recipe.AddIngredient(ItemID.SoulofSight, 4); 	
+			recipe.AddIngredient(ItemID.TitaniumBar, 2); 
+			recipe.AddIngredient(ItemID.MythrilBar, 2);  	
             recipe.AddTile(TileID.MythrilAnvil); 			
             recipe.SetResult(this);
             recipe.AddRecipe(); 
