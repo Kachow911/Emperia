455ac2cf85bd6b2f988d131ca4df05ebe280926d
Miningdiamonds8 <smilecupcake8@gmail.com>
2019-11-13 17:20:07 -0500

    Update Frozen.cs

--- Buffs/Frozen.cs
+++ Buffs/Frozen.cs
@@ -21,7 +21,7 @@ namespace Emperia.Buffs
 
         public override void Update(NPC npc, ref int buffIndex)
         {
-            npc.GetGlobalNPC<MyNPC>(mod).burningNight = true;    //this tells the game to use the public bool customdebuff from NPCsINFO.cs
+            npc.GetGlobalNPC<MyNPC>().burningNight = true;    //this tells the game to use the public bool customdebuff from NPCsINFO.cs
             int num1 = Dust.NewDust(npc.position, npc.width, npc.height, 15);    //this is the dust/flame effect that will apear on npc or player if is hit by this buff   
             Main.dust[num1].noGravity = true;
             Main.dust[num1].velocity *= 2f;
