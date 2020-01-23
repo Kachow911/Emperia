201307bdafdbe348ab32fe3191d78b6e7d4c89f8
Torniky <44421508+Torniky@users.noreply.github.com>
2020-01-22 21:09:44 -0800

    dd

---
+++ Items/Sets/PreHardmode/Granite/GraniteCanister.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Emperia.Items.Sets.PreHardmode.Granite
+{
+	public class GraniteCanister : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Granite Canister");
+			Tooltip.SetDefault("Creates a forcefield to do something");
+		}
+
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.width = 16;
+            item.height = 16;
+            item.noUseGraphic = true;
+            item.UseSound = SoundID.Item1;
+            item.thrown = true;
+            item.channel = true;
+            item.noMelee = true;
+            item.shoot = mod.ProjectileType("GraniteCanisterProj");
+            item.useAnimation = 25;
+            item.useTime = 25;
+            item.shootSpeed = 8.0f;
+            item.knockBack = 3.5f;
+			item.value = Item.sellPrice(0, 0, 1, 50);
+            item.crit = 4;
+            item.rare = 2;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+        }
+		
+    }
+}
---
+++ Projectiles/GraniteCanisterProj.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace Emperia.Projectiles
+{
+	
+	public class GraniteCanisterProj : ModProjectile
+	{
+		private bool hitGround;
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.PainterPaintball);
+			projectile.friendly = true;
+			projectile.penetrate = -1;
+			projectile.timeLeft = 225;
+			projectile.alpha = 0;
+			projectile.damage = 0;
+		}
+		public override void AI()
+		{
+			projectile.alpha = 0;
+			if (Main.rand.NextBool(20))
+			{
+				Color rgb = new Color(135,206,250);
+				int index2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 240, (float) projectile.velocity.X, (float) projectile.velocity.Y, 0, rgb, 0.9f);
+			}
+			projectile.velocity.Y += .3f;
+			if (!hitGround)
+				projectile.rotation += Main.rand.Next(10) * .01f;
+		}
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.alpha = 255;
+			if (!hitGround)
+			{
+				Gore.NewGore(projectile.position, new Vector2(Main.rand.Next(-2, 2), -5), mod.GetGoreSlot("Gores/GraniteCanister"), 1f);
+			}	
+			hitGround = true;
+			projectile.velocity = Vector2.Zero;
+			
+			return false;
+			
+		}
+		public override bool PreDrawExtras(SpriteBatch spriteBatch)
+		{
+
+			return true;
+		}
+	}
+}
