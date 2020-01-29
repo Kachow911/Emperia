ee7b6bb8e3d7a684147d6e4dde7040a4bf1f1c86
Miningdiamonds8 <smilecupcake8@gmail.com>
2020-01-28 21:07:46 -0500

    Add files via upload

---
+++ Buffs/Waxwing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace Emperia.Buffs
+{
+    public class Waxwing : ModBuff
+    {
+        public override void SetDefaults()
+        {
+			DisplayName.SetDefault("Waxwing");
+			Description.SetDefault("25% increased wing speed, but 15% decreased flight time");
+            Main.buffNoSave[Type] = true;
+            canBeCleared = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+			MyPlayer p = player.GetModPlayer<MyPlayer>();
+			player.wingTime *= 0.85f;
+            if (player.wingTime > 0f)
+            {
+                string speedText = player.velocity.X.ToString();
+                Main.NewText(speedText, 255, 240, 20, false);
+                //player.moveSpeed *= 1.25f;
+                //player.maxRunSpeed *= 1.25f;
+                //player.velocity.X -= (player.velocity.X / 4);
+            }
+        }
+    }
+}
