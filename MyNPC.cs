ef20ed7958e9c1249a2a59313b7e5cef2c97cf6e
Miningdiamonds8 <smilecupcake8@gmail.com>
2020-01-28 21:23:02 -0500

    Update MyNPC.cs

--- MyNPC.cs
+++ MyNPC.cs
@@ -215,6 +215,13 @@ namespace Emperia
 					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("TetheredPiranha")); 
 				}
 			}
+			if(npc.type == 122)
+			{
+				if (Main.rand.Next(40) == 0) 
+				{
+					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Escargun")); 
+				}
+			}
 			if (fatesDemise)
 			{
 				
