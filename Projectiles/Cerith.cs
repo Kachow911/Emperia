e50064ea186179248b71911cb9456b9c1c9d295e
Miningdiamonds8 <smilecupcake8@gmail.com>
2020-02-02 01:35:27 -0500

    Update Cerith.cs

--- Projectiles/Cerith.cs
+++ Projectiles/Cerith.cs
@@ -15,32 +15,30 @@ namespace Emperia.Projectiles
 			DisplayName.SetDefault("Cerith");
 		}
         public override void SetDefaults()
-        {  //projectile name
-            projectile.width = 8;       //projectile width
-            projectile.height = 8;  //projectile height
-            projectile.friendly = true;      //make that the projectile will not damage you
-            projectile.magic = true;         // 
-            projectile.tileCollide = true;   //make that the projectile will be destroed if it hits the terrain
-            projectile.penetrate = 1;      //how many npc will penetrate
-            projectile.timeLeft = 150;   //how many time projectile projectile has before disepire
+        {
+            projectile.width = 12;
+            projectile.height = 12;
+            projectile.friendly = true;
+            projectile.magic = true;
+            projectile.tileCollide = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 140;
             projectile.extraUpdates = 1;
             projectile.ignoreWater = true;
-			
         }
-        public override void AI()           //projectile make that the projectile will face the corect way
-        {                                                           // |
+        public override void AI()
+        {
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
-		
 		}
 		public override void Kill(int timeLeft)
 		{
+            Main.PlaySound(SoundID.Item10, projectile.position);
 			for (int i = 0; i < 5; i++)
 			{
 				int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 7);
 				Vector2 vel = new Vector2(0, -1).RotatedBy(Main.rand.NextFloat() * 6.283f) * 3.5f;
 			}
 		}
-	
         
     }
 }
