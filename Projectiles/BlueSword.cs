0fed27329c52b6b729d13565fd5fccacfcd4c267
Miningdiamonds8 <smilecupcake8@gmail.com>
2020-02-14 21:32:22 -0500

    Add files via upload

--- Projectiles/BlueSword.cs
+++ Projectiles/BlueSword.cs
@@ -12,36 +12,40 @@ namespace Emperia.Projectiles
     {
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Blue Day's Dlade");
+			DisplayName.SetDefault("Blue Day's Blade");
 		}
         public override void SetDefaults()
-        {  //projectile name
-            projectile.width = 38;       //projectile width
-            projectile.height = 38;  //projectile height
-            projectile.friendly = true;      //make that the projectile will not damage you
-            projectile.melee = true;         // 
-            projectile.tileCollide = false;   //make that the projectile will be destroed if it hits the terrain
-            projectile.penetrate = 2;      //how many npc will penetrate
-            projectile.timeLeft = 400;   //how many time projectile projectile has before disepire
-            projectile.light = 0.75f;    // projectile light
+        {
+            projectile.width = 18;
+            projectile.height = 18;
+            projectile.friendly = true;   
+            projectile.melee = true;      
+            projectile.tileCollide = false;
+            projectile.penetrate = 2;     
+            projectile.timeLeft = 400;
+            projectile.light = 0.75f;
             projectile.ignoreWater = true;
         }
-        public override void AI()           //projectile make that the projectile will face the corect way
-        {                                                           // |
+        public override void AI()
+        {
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 0.785f;
 			projectile.alpha = 100 + (int) (Math.Cos(projectile.timeLeft) * 100);
 			if(Main.rand.Next(2) == 0)
 			{
-				int num250 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 66, (float)(projectile.direction * 2), 0f, 150, new Color(53f, 67f, 253f), 1.3f);
+				int num250 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 187, (float)(projectile.direction * 2), 0f, 150, new Color(53f, 67f, 253f), 1.3f);
 				Main.dust[num250].noGravity = true;
 				Main.dust[num250].velocity *= 0f;
 			}
 			Player player = Main.player[projectile.owner];
-			if (projectile.Center.Y > player.Center.Y - player.height / 2)
+			if (projectile.Center.Y > player.Center.Y - player.height * 4)
 			{
 				projectile.tileCollide = true;
 			}
         }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{	
+			target.immune[projectile.owner] = 5;
+		}
 		public override void Kill(int timeLeft)
         {
 			Main.PlaySound(SoundID.Item10, projectile.position);
@@ -49,15 +53,15 @@ namespace Emperia.Projectiles
             {
               float num1 = (float) (projectile.oldVelocity.X * (30.0 / (double) index1));
               float num2 = (float) (projectile.oldVelocity.Y * (30.0 / (double) index1));
-              int index2 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 187, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, Color.LightBlue, 1.8f);
+              int index2 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 187, (float) projectile.oldVelocity.X * 2, (float) projectile.oldVelocity.Y * 2, 100, Color.LightBlue, 2f);
               Main.dust[index2].noGravity = true;
               Dust dust1 = Main.dust[index2];
               dust1.velocity = dust1.velocity * 0.5f;
-              int index3 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 187, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, Color.LightBlue, 1.4f);
+              int index3 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 187, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, Color.LightBlue, 1.6f);
+              Main.dust[index3].noGravity = true;
               Dust dust2 = Main.dust[index3];
               dust2.velocity = dust2.velocity * 0.5f;
             }
-		
         }
     }
 }
--- Projectiles/PinkSword.cs
+++ Projectiles/PinkSword.cs
@@ -15,34 +15,45 @@ namespace Emperia.Projectiles
 			DisplayName.SetDefault("Pink Day's Blade");
 		}
         public override void SetDefaults()
-        {  //projectile name
-            projectile.width = 38;       //projectile width
-            projectile.height = 38;  //projectile height
-            projectile.friendly = true;      //make that the projectile will not damage you
-            projectile.melee = true;         // 
-            projectile.tileCollide = false;   //make that the projectile will be destroed if it hits the terrain
-            projectile.penetrate = 3;      //how many npc will penetrate
-            projectile.timeLeft = 400;   //how many time this projectile has before disepire
-            projectile.light = 0.75f;    // projectile light
+        {
+            projectile.width = 18;
+            projectile.height = 18;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.penetrate = 3;
+            projectile.timeLeft = 400;
+            projectile.light = 0.75f;
             projectile.extraUpdates = 1;
             projectile.ignoreWater = true;
         }
-        public override void AI()           //this make that the projectile will face the corect way
-        {                                                           // |
+        public override void AI()
+        {
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 0.785f;
 			projectile.alpha = 100 + (int) (Math.Cos(projectile.timeLeft) * 100);
 			if(Main.rand.Next(2) == 0)
 			{
-            int num250 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 66, (float)(projectile.direction * 2), 0f, 150, new Color(53f, 67f, 253f), 1.3f);
+            int num250 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 255, (float)(projectile.direction * 2), 0f, 150, new Color(53f, 67f, 253f), 1.3f);
 					Main.dust[num250].noGravity = true;
 					Main.dust[num250].velocity *= 0f;
 			}
 			Player player = Main.player[projectile.owner];
-			if (projectile.Center.Y > player.Center.Y - player.height / 2)
+			if (projectile.Center.Y > player.Center.Y - player.height * 4)
 			{
 				projectile.tileCollide = true;
 			}
         }
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) {
+            Player player = Main.player[projectile.owner];
+			if (!crit && Main.rand.Next(100 - player.meleeCrit) < 4)
+            {
+				crit = true;
+			}
+		}
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{	
+			target.immune[projectile.owner] = 5;
+		}
 		public override void Kill(int timeLeft)
         {
 			 Main.PlaySound(SoundID.Item10, projectile.position);
@@ -50,17 +61,15 @@ namespace Emperia.Projectiles
             {
               float num1 = (float) (projectile.oldVelocity.X * (30.0 / (double) index1));
               float num2 = (float) (projectile.oldVelocity.Y * (30.0 / (double) index1));
-              int index2 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 58, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, Color.Pink, 1.8f);
+              int index2 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 255, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, default(Color), 1.6f);
               Main.dust[index2].noGravity = true;
               Dust dust1 = Main.dust[index2];
               dust1.velocity = dust1.velocity * 0.5f;
-              int index3 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 58, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, Color.Pink, 1.4f);
+              int index3 = Dust.NewDust(new Vector2((float) projectile.oldPosition.X - num1, (float) projectile.oldPosition.Y - num2), 8, 8, 255, (float) projectile.oldVelocity.X, (float) projectile.oldVelocity.Y, 100, default(Color), 1.3f);
+              Main.dust[index3].noGravity = true;
               Dust dust2 = Main.dust[index3];
               dust2.velocity = dust2.velocity * 0.5f;
             }
-		
-		
         }
-	
     }
 }
