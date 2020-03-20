9cec317f9c11c171d390da53ddba60c29700c49e
Torniky <44421508+Torniky@users.noreply.github.com>
2020-03-19 20:39:22 -0700

    BloodboilStaff

---
+++ Items/Sets/Hardmode/Crimson/BloodboilStaff.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System;
+
+namespace Emperia.Items.Sets.Hardmode.Crimson
+{
+	public class BloodboilStaff : ModItem
+	{
+		public override void SetDefaults()
+		{
+
+			item.damage = 24;
+			item.magic = true;
+			item.mana = 5;
+			item.width = 25;
+			item.height = 26;
+			item.useTime = 15;
+			item.UseSound = SoundID.Item43;
+			item.useAnimation = 15;
+			item.reuseDelay = 14;
+			item.useStyle = 5;
+			Item.staff[item.type] = true;
+			item.noMelee = true;
+			item.knockBack = 2.5f;
+			item.value = 6500;
+			item.rare = 4;
+			item.autoReuse = true;
+			item.shoot = mod.ProjectileType("BigBubble");
+			item.shootSpeed = 12f;
+		}
+		
+
+		public override void SetStaticDefaults()
+		{
+		  //DisplayName.SetDefault("Tesla Coil Rod");
+		  Tooltip.SetDefault("Fires splitting Ichor bubbles");
+		}
+		
+		/*public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "JungleMaterial", 5);
+			recipe.AddIngredient(ItemID.AdamantiteBar, 2);
+			recipe.AddIngredient(ItemID.SoulofNight, 2);
+			recipe.AddIngredient(ItemID.SoulofLight, 2);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "JungleMaterial", 5);
+			recipe.AddIngredient(ItemID.TitaniumBar, 2);
+			recipe.AddIngredient(ItemID.SoulofNight, 2);
+			recipe.AddIngredient(ItemID.SoulofLight, 2);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}*/
+	}
+}
---
+++ Projectiles/Crimson/IchorBubbles.cs
@@ -0,0 +1,157 @@
+using Terraria;
+using System;
+using Terraria.ID;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace Emperia.Projectiles.Crimson
+{
+
+    public class BigBubble : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ichor Bubble");
+        }
+        public override void SetDefaults()
+        {  //projectile name
+            projectile.width = 12;       //projectile width
+            projectile.height = 12;  //projectile height
+            projectile.friendly = true;      //make that the projectile will not damage you
+            projectile.magic = true;         // 
+            projectile.tileCollide = true;   //make that the projectile will be destroed if it hits the terrain
+            projectile.penetrate = 1;      //how many npc will penetrate
+            projectile.timeLeft = 80;
+            projectile.light = 0.75f;    // projectile light
+            projectile.ignoreWater = true;
+            projectile.alpha = 100;
+        }
+        public override void AI()           //projectile make that the projectile will face the corect way
+        {
+
+            projectile.velocity.X *= .98f;
+
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(2) == 0)
+                target.AddBuff(BuffID.Ichor, 300);
+        }
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(SoundID.Item54, projectile.Center);
+            for (int i = 0; i < 2; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(0, 3).RotatedByRandom(MathHelper.ToRadians(360));
+                Projectile.NewProjectile(projectile.position.X, projectile.position.Y, perturbedSpeed.X, perturbedSpeed.Y, mod.ProjectileType("MedBubble"), projectile.damage / 2, projectile.knockBack, projectile.owner, 0, 0);
+            }
+            for (int i = 0; i < 360; i++)
+            {
+                Vector2 vec = Vector2.Transform(new Vector2(-6, 0), Matrix.CreateRotationZ(MathHelper.ToRadians(i)));
+                if (i % 8 == 0)
+                {
+                   int b = Dust.NewDust(projectile.Center + vec, Main.rand.Next(1, 7), Main.rand.Next(1, 7), 87);
+                    Main.dust[b].noGravity = true;
+                    Main.dust[b].velocity = Vector2.Zero;
+                }
+            }
+        }
+    }
+    public class MedBubble : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ichor Bubble");
+        }
+        public override void SetDefaults()
+        {  //projectile name
+            projectile.width = 12;       //projectile width
+            projectile.height = 12;  //projectile height
+            projectile.friendly = true;      //make that the projectile will not damage you
+            projectile.magic = true;         // 
+            projectile.tileCollide = true;   //make that the projectile will be destroed if it hits the terrain
+            projectile.penetrate = 1;      //how many npc will penetrate
+            projectile.timeLeft = 80;
+            projectile.light = 0.75f;    // projectile light
+            projectile.ignoreWater = true;
+            projectile.alpha = 100;
+        }
+        public override void AI()           //projectile make that the projectile will face the corect way
+        {
+
+            projectile.velocity.X *= .99f;
+
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(3) == 0)
+                target.AddBuff(BuffID.Ichor, 300);
+        }
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(SoundID.Item54, projectile.Center);
+            for (int i = 0; i < 2; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(0, 3).RotatedByRandom(MathHelper.ToRadians(360));
+                Projectile.NewProjectile(projectile.position.X, projectile.position.Y, perturbedSpeed.X, perturbedSpeed.Y, mod.ProjectileType("SmallBubble"), projectile.damage / 2, projectile.knockBack, projectile.owner, 0, 0);
+            }
+            for (int i = 0; i < 360; i++)
+            {
+                Vector2 vec = Vector2.Transform(new Vector2(-4, 0), Matrix.CreateRotationZ(MathHelper.ToRadians(i)));
+                if (i % 8 == 0)
+                {
+                    int b = Dust.NewDust(projectile.Center + vec, Main.rand.Next(1, 7), Main.rand.Next(1, 7), 87);
+                    Main.dust[b].noGravity = true;
+                    Main.dust[b].velocity = Vector2.Zero;
+                }
+            }
+        }
+
+    }
+    public class SmallBubble : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ichor Bubble");
+        }
+        public override void SetDefaults()
+        {  //projectile name
+            projectile.width = 12;       //projectile width
+            projectile.height = 12;  //projectile height
+            projectile.friendly = true;      //make that the projectile will not damage you
+            projectile.magic = true;         // 
+            projectile.tileCollide = true;   //make that the projectile will be destroed if it hits the terrain
+            projectile.penetrate = 1;      //how many npc will penetrate
+            projectile.timeLeft = 80;
+            projectile.light = 0.75f;    // projectile light
+            projectile.ignoreWater = true;
+            projectile.alpha = 100;
+        }
+        public override void AI()           //projectile make that the projectile will face the corect way
+        {
+
+
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(5) == 0)
+                target.AddBuff(BuffID.Ichor, 300);
+        }
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(SoundID.Item54, projectile.Center);
+            for (int i = 0; i < 360; i++)
+            {
+                Vector2 vec = Vector2.Transform(new Vector2(-2, 0), Matrix.CreateRotationZ(MathHelper.ToRadians(i)));
+                if (i % 8 == 0)
+                {
+                    int b = Dust.NewDust(projectile.Center + vec, Main.rand.Next(1, 7), Main.rand.Next(1, 7), 87);
+                    Main.dust[b].noGravity = true;
+                    Main.dust[b].velocity = Vector2.Zero;
+                }
+            }
+        }
+
+    }
+}
